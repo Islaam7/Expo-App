bc153543ff0e6cbfa7a1b491b6fc7765cd39be93
Eslam Fares Mohamed Emam <152084165+bgbos7077@users.noreply.github.com>
2024-04-23 10:13:10 +0200

    "Rename iindex.tsx to home.tsx and update TabLayout to use 'home' instead of 'iindex'"
    This commit renames the file `iindex.tsx` to `home.tsx`

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -27,7 +27,7 @@ export default function TabLayout() {
         headerShown: useClientOnlyValue(false, true),
       }}>
       <Tabs.Screen
-        name="iindex"
+        name="home"
         options={{
           title: 'Home',
           tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
